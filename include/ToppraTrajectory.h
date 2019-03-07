7a3f50edbb18d7465350ac5cc2d9b6ecb95da725
aivano <antun.ivanovic@fer.hr>
2019-03-07 14:55:25 +0100

    Add doxygen comments to ToppraTrajectory.h

--- include/ToppraTrajectory.h
+++ include/ToppraTrajectory.h
@@ -1,6 +1,4 @@
 /// \file ToppraTrajectory.h
-/// \brief Contains the implementation for time optimal path parametrization
-/// with reachability analysis trajectory generation algorithm
 
 #ifndef TOPPRA_TRAJECTORY_H
 #define TOPPRA_TRAJECTORY_H
@@ -20,6 +18,9 @@
 
 using namespace std;
 
+/// \brief Contains the implementation for time optimal path parametrization 
+///        with reachability analysis trajectory generation algorithm.
+
 /// This class handles the implementation of TOPP-RA trajectory. Since this
 /// algorithm is implemented in Python, this class will communicate with the 
 /// ROS node that generates trajectory through service. Therefore, this class
@@ -28,10 +29,31 @@ using namespace std;
 class ToppraTrajectory : public TrajectoryInterface
 {
   public:
+    /// \brief Constructor with filename.
+    /// \param config_filename This constructor takes .yaml file configuration for constraints and
+    ///   sampling frequency.
     ToppraTrajectory(string config_filename);
+
+    /// \brief Constructor with config_matrix
+    /// \param config_matrix This constructor takes configuration matrix for velocity and acceleration
+    ///   contstraints. First row of matrix corresponds to velocity constraints, 
+    ///   second row to acceleration constraints. Both rows must have the same
+    ///   number of elements.
     ToppraTrajectory(Eigen::MatrixXd config_matrix);
+
+    /// \brief Destructor.
     ~ToppraTrajectory();
+
+    /// \brief Generates TOPP-RA trajectory through specified waypoints taking
+    ///   constraints into consideration.
+    /// \param positions The waypoints are passed through a matrix. Each row of matrix is one
+    ///   one waypoint. Number of columns represents the degrees of freedom.
+    /// \return Success of trajectory generation.
     bool generateTrajectory(Eigen::MatrixXd positions);
+
+    /// \brief Returns the generated trajectory.
+    ///
+    /// \return Sampled trajectory.
     Trajectory getTrajectory();
 
   private:
