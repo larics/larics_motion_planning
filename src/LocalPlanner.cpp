94829b0a804731226df96ba935d6b7805d754a4f
aivano <antun.ivanovic@fer.hr>
2019-09-11 17:09:12 +0200

    [Local planner] Add skeleton code for local planner class.

---
+++ src/LocalPlanner.cpp
@@ -0,0 +1,11 @@
+#include <larics_motion_planning/LocalPlanner.h>
+
+LocalPlanner::LocalPlanner(string config_filename)
+{
+
+}
+
+bool LocalPlanner::configureFromFile(string config_filename)
+{
+  
+}
