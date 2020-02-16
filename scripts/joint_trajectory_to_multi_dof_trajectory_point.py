bdb6710e93087cd17651f4280eea5970eee79b33
aivano <antun.ivanovic@fer.hr>
2020-02-16 23:29:08 +0100

    [TrajectoryDequeue] Add delta position and velocity publishers.

--- scripts/joint_trajectory_to_multi_dof_trajectory_point.py
+++ scripts/joint_trajectory_to_multi_dof_trajectory_point.py
@@ -3,8 +3,8 @@
 __author__ = 'aivanovic'
 
 import rospy, math
-from math import sin, cos
-from geometry_msgs.msg import Twist, Transform
+from math import sin, cos, sqrt
+from geometry_msgs.msg import Twist, Transform, Pose, PoseStamped, TwistStamped
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Float64, Empty, Int32, Float32
 from trajectory_msgs.msg import MultiDOFJointTrajectoryPoint, \
@@ -25,8 +25,20 @@ class JointTrajectoryToMultiDofTrajectoryPoint:
         self.airdrop_flag = rospy.get_param('~airdrop', False)
         if self.airdrop_flag == True:
             self.magnet_pub = rospy.Publisher('magnet/gain', Float32, queue_size=1)
+            self.dropoff_delta_pub = rospy.Publisher('dropoff_delta/position', Pose, queue_size=1)
+            self.dropoff_delta_velocity_pub = rospy.Publisher('dropoff_delta/velocity', 
+                Twist, queue_size=1)
+            self.airdrop_pose = Pose()
+            self.airdrop_velocity = Twist()
             self.magnet_gain = 1.0
             self.airdrop_counter = 0
+            self.uav_current_pose = Pose()
+            self.uav_current_velocity = Twist()
+            #rospy.Subscriber('odometry', Odometry, self.uavOdometryCallback, queue_size=1)
+            rospy.Subscriber('pose', PoseStamped, self.uavPoseCallback, queue_size=1)
+            rospy.Subscriber('velocity_relative', TwistStamped, self.uavVelocityCallback, queue_size=1)
+            self.delta = 1000
+            self.delta_previous = 1000
         self.t_start = rospy.Time.now()
         self.executing_trajectory_flag = False
         self.joint_trajectory = JointTrajectory()
@@ -50,15 +62,43 @@ class JointTrajectoryToMultiDofTrajectoryPoint:
                 self.uav_current_trajectory_point = jointTrajectoryPointToMultiDofJointTrajectoryPoint(
                     self.current_trajectory_point)
                 if self.airdrop_flag == True:
+                    airdrop_delta = Pose()
+                    airdrop_delta.position.x = abs(self.airdrop_pose.position.x - 
+                        self.uav_current_pose.position.x)
+                    airdrop_delta.position.y = abs(self.airdrop_pose.position.y - 
+                        self.uav_current_pose.position.y)
+                    airdrop_delta.position.z = abs(self.airdrop_pose.position.z - 
+                        self.uav_current_pose.position.z)
+                    airdrop_delta.orientation.w = sqrt(airdrop_delta.position.x**2 + 
+                        airdrop_delta.position.y**2 + airdrop_delta.position.z**2)
+                    airdrop_delta.orientation.x = bool(self.airdrop_counter)
+                    self.delta_previous = self.delta
+                    self.delta = airdrop_delta.orientation.w
+
+                    airdrop_delta_velocity = Twist()
+                    airdrop_delta_velocity.linear.x = abs(self.airdrop_velocity.linear.x - 
+                        self.uav_current_velocity.linear.x)
+                    airdrop_delta_velocity.linear.y = abs(self.airdrop_velocity.linear.y - 
+                        self.uav_current_velocity.linear.y)
+                    airdrop_delta_velocity.linear.z = abs(self.airdrop_velocity.linear.z - 
+                        self.uav_current_velocity.linear.z)
+                    airdrop_delta_velocity.angular.z = sqrt(airdrop_delta_velocity.linear.x**2 + 
+                        airdrop_delta_velocity.linear.y**2 + airdrop_delta_velocity.linear.z**2)
+                    airdrop_delta_velocity.angular.x = bool(self.airdrop_counter)
+
                     if abs(self.joint_trajectory.points[0].positions[4]) < 0.1:
                         self.magnet_gain = 1.0
                     elif abs(self.joint_trajectory.points[0].positions[4] - 1.0) < 0.1:
                         self.airdrop_counter = self.airdrop_counter + 1
-                    if self.airdrop_counter >= 1:
+                    if self.airdrop_counter >= 1 and (self.delta < 0.025 or 
+                        self.delta > self.delta_previous):
                         self.magnet_gain = 0.0
                         if ball_released_flag == False:
                             rospy.loginfo("Ball released")
                             ball_released_flag = True
+
+                    self.dropoff_delta_pub.publish(airdrop_delta)
+                    self.dropoff_delta_velocity_pub.publish(airdrop_delta_velocity)
                     self.magnet_pub.publish(self.magnet_gain)
                 self.publishAll()
                 #self.joint_trajectory_point_pub.publish(self.current_trajectory_point)
@@ -83,8 +123,29 @@ class JointTrajectoryToMultiDofTrajectoryPoint:
             len(msg.points) > 0:
             self.joint_trajectory = copy.deepcopy(msg)
             self.executing_trajectory_flag = True
+            if self.airdrop_flag == True:
+                for i in range(len(msg.points)):
+                    if msg.points[i].positions[4] == 1:
+                        self.airdrop_pose.position.x = msg.points[i].positions[0]
+                        self.airdrop_pose.position.y = msg.points[i].positions[1]
+                        self.airdrop_pose.position.z = msg.points[i].positions[2]
+                        self.airdrop_velocity.linear.x = msg.points[i].velocities[0]
+                        self.airdrop_velocity.linear.y = msg.points[i].velocities[1]
+                        self.airdrop_velocity.linear.z = msg.points[i].velocities[2]
+                        break
         else:
             print "Currently executing a trajectory."
+
+    def uavOdometryCallback(self,msg):
+        self.uav_current_pose = msg.pose.pose
+        # This is velocity in uav frame
+        self.uav_current_velocity = msg.twist.twist
+
+    def uavPoseCallback(self,msg):
+        self.uav_current_pose = msg.pose
+
+    def uavVelocityCallback(self,msg):
+        self.uav_current_velocity = msg.twist
 
     def publishAll(self):
         self.uav_trajectory_point_pub.publish(self.uav_current_trajectory_point)
