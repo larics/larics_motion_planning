50770d9a81fd37a84b80783fa37e23cf63cd6d3c
aivano <antun.ivanovic@fer.hr>
2020-07-06 16:09:13 +0200

    [Scripts] Plan trajectory with preplanned parabola config files.

--- scripts/red_plan_airdrop_from_current_reference.py
+++ scripts/red_plan_airdrop_from_current_reference.py
@@ -2,9 +2,10 @@
 
 __author__ = 'aivanovic'
 
-import rospy, math
+import rospy, math, rospkg
 from math import sin, cos
-from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped, Vector3
+from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped, \
+    Vector3, Quaternion
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Float64, Empty, Int32, Float32
 from trajectory_msgs.msg import MultiDOFJointTrajectoryPoint, \
@@ -12,6 +13,7 @@ from trajectory_msgs.msg import MultiDOFJointTrajectoryPoint, \
 from larics_motion_planning.srv import ParabolicAirdropTrajectory, \
     ParabolicAirdropTrajectoryRequest, ParabolicAirdropTrajectoryResponse
 import copy
+import numpy as np
 
 class PlanAirdropFromCurrentReference:
 
@@ -23,6 +25,12 @@ class PlanAirdropFromCurrentReference:
             JointTrajectory, queue_size=1)
 
         self.rate = rospy.get_param('~rate', 10)
+        self.filename = rospy.get_param('~repeatability_config_file', 
+            str('red_repeatability_configs.csv'))
+        rospack = rospkg.RosPack()
+        self.filename = rospack.get_path('larics_motion_planning') + \
+            '/config/airdrop_configs/' + self.filename
+        self.airdrop_configs = np.loadtxt(open(self.filename, "rb"), delimiter=",")
 
         self.first_reference_received = False
         self.plan_to_target = False
@@ -33,17 +41,10 @@ class PlanAirdropFromCurrentReference:
             queue_size=1)
         rospy.Subscriber('parabolic_airdrop/plan_to_target', Vector3, 
             self.targetCallback, queue_size=1)
+        rospy.Subscriber('parabolic_airdrop/id_plan_to_target', Quaternion, 
+            self.idTargetCallback, queue_size=1)
 
     def run(self):
-        self.airdrop_request.publish_path = True
-        self.airdrop_request.publish_trajectory = True
-        self.airdrop_request.plan_path = True
-        self.airdrop_request.plan_trajectory = True
-        self.airdrop_request.use_custom_parabola_params = False
-        self.airdrop_request.use_custom_psi_params = True
-        self.airdrop_request.custom_psi_params = [-14, 2, -14]
-
-
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             rate.sleep()
@@ -58,8 +59,42 @@ class PlanAirdropFromCurrentReference:
                 response = self.parabolic_airdrop_service(self.airdrop_request)
 
     def targetCallback(self, msg):
+        self.airdrop_request.publish_path = True
+        self.airdrop_request.publish_trajectory = True
+        self.airdrop_request.plan_path = True
+        self.airdrop_request.plan_trajectory = True
+        self.airdrop_request.use_custom_parabola_params = False
+        self.airdrop_request.use_custom_psi_params = True
+        self.airdrop_request.custom_psi_params = [-14, 2, -14]
+
         self.target.position = msg
         self.plan_to_target = True
+
+    def idTargetCallback(self, msg):
+        # Read parabola params from file
+        idd = int(msg.w)
+        if idd >= self.airdrop_configs.shape[0]:
+            self.plan_to_target = False
+            print("Cannot plan to target. Index out of scope.")
+        else:
+            v0 = self.airdrop_configs[idd][0]
+            dx = self.airdrop_configs[idd][1]
+            alpha = self.airdrop_configs[idd][2]
+            dz = self.airdrop_configs[idd][3]
+            psi = self.airdrop_configs[idd][4]
+            self.airdrop_request.publish_path = False
+            self.airdrop_request.publish_trajectory = True
+            self.airdrop_request.plan_path = False
+            self.airdrop_request.plan_trajectory = True
+            self.airdrop_request.use_custom_parabola_params = True
+            self.airdrop_request.custom_parabola_params = [v0, dz, alpha, dx, psi]
+            self.airdrop_request.use_custom_psi_params = False
+            print self.airdrop_request
+
+            self.target.position.x = msg.x
+            self.target.position.y = msg.y
+            self.target.position.z = msg.z
+            self.plan_to_target = True
 
     def currentReferenceCallback(self, msg):
         self.uav_current_reference.position.x = msg.transforms[0].translation.x
