eae2e953739492ab2c342033b5701fd9aeff0adb
aivano <antun.ivanovic@fer.hr>
2021-01-04 09:26:16 +0100

    [Scripts] Add plant box and direction arrows visualization.

--- scripts/greenhouse/plant_box_inspection_points.py
+++ scripts/greenhouse/plant_box_inspection_points.py
@@ -18,6 +18,7 @@ class BoxInspectionPoints:
     # Params for box dimensions
     self.lx = rospy.get_param('~box_length', 0.9)
     self.ly = rospy.get_param('~box_width', 0.63)
+    self.lz = rospy.get_param('~box_height', 0.2)
     # Get r1 and r2 distances from box during inspection
     self.r1 = rospy.get_param('~r1', 1.0)
     self.r2 = rospy.get_param('~r2', 0.5)
@@ -61,7 +62,8 @@ class BoxInspectionPoints:
     marker.header.frame_id = "world"
 
     # First add inspection points as sphere list
-    marker.id = 1
+    marker.id = 0
+    marker.ns = "inspection points"
     marker.type = Marker.SPHERE_LIST
     marker.action = marker.ADD
     marker.scale.x = 0.1
@@ -72,9 +74,76 @@ class BoxInspectionPoints:
     marker.color.b = 12.0/255.0
     marker.color.a = 1.0
     marker.lifetime = rospy.Duration()
+    marker.pose.orientation.w = 1.0
     for i in range(len(self.T_points)):
       marker.points.append(numpyMatrixToPoint(self.T_points[i]))
     self.marker_array.markers.append(copy.deepcopy(marker))
+
+    # Next add the plant box
+    marker.id = len(self.marker_array.markers)
+    marker.ns = "plant box"
+    marker.type = Marker.CUBE
+    marker.action = marker.ADD
+    marker.scale.x = self.lx
+    marker.scale.y = self.ly
+    marker.scale.z = self.lz
+    marker.color.r = 37.0/255.0
+    marker.color.g = 194.0/255.0
+    marker.color.b = 79.0/255.0
+    marker.color.a = 0.7
+    marker.lifetime = rospy.Duration()
+    marker.points = [] # Reset points
+    marker.pose.position.x = self.box_config_vector[0]
+    marker.pose.position.y = self.box_config_vector[1]
+    marker.pose.position.z = self.box_config_vector[2]
+    marker.pose.orientation.z = sin(self.box_config_vector[3]/2.0)
+    marker.pose.orientation.w = cos(self.box_config_vector[3]/2.0)
+    self.marker_array.markers.append(copy.deepcopy(marker))
+
+    # Next add the plant box centroid
+    marker.id = len(self.marker_array.markers)
+    marker.ns = "plant box center"
+    marker.type = Marker.SPHERE
+    marker.action = marker.ADD
+    marker.scale.x = 0.1
+    marker.scale.y = 0.1
+    marker.scale.z = 0.01
+    marker.color.r = 51.0/255.0
+    marker.color.g = 41.0/255.0
+    marker.color.b = 207.0/255.0
+    marker.color.a = 1.0
+    marker.lifetime = rospy.Duration()
+    marker.points = [] # Reset points
+    marker.pose.position.x = self.box_config_vector[0]
+    marker.pose.position.y = self.box_config_vector[1]
+    marker.pose.position.z = self.box_config_vector[2] + self.lz/2.0
+    marker.pose.orientation.z = sin(self.box_config_vector[3]/2.0)
+    marker.pose.orientation.w = cos(self.box_config_vector[3]/2.0)
+    self.marker_array.markers.append(copy.deepcopy(marker))
+
+    # Add arrows pointing the inspection direction
+    for i in range(3):
+      marker.id = len(self.marker_array.markers)
+      marker.ns = "Directions"
+      marker.type = Marker.ARROW
+      marker.action = marker.ADD
+      marker.scale.x = 0.5
+      marker.scale.y = 0.04
+      marker.scale.z = 0.04
+      marker.color.r = 191.0/255.0
+      marker.color.g = 25.0/255.0
+      marker.color.b = 58.0/255.0
+      marker.color.a = 1.0
+      marker.lifetime = rospy.Duration()
+      marker.points = [] # Reset points
+      temp_point = numpyMatrixToPoint(self.T_points[i*2])
+      marker.pose.position.x = temp_point.x
+      marker.pose.position.y = temp_point.y
+      marker.pose.position.z = temp_point.z
+      alpha = atan2(self.T_points[i*2][1][0], self.T_points[i*2][0][0])
+      marker.pose.orientation.z = sin(alpha/2.0)
+      marker.pose.orientation.w = cos(alpha/2.0)
+      self.marker_array.markers.append(copy.deepcopy(marker))
 
 
   def getInspectionPoints(self, box_vector):
