95fb7dea2a14139cdd698f363f9071beacd83064
aivano <antun.ivanovic@fer.hr>
2021-03-10 14:20:28 +0100

    [Scripts] Add continuous reference publisher.

--- scripts/joint_trajectory_reference_tracker.py
+++ scripts/joint_trajectory_reference_tracker.py
@@ -7,15 +7,22 @@ from std_msgs.msg import Int32
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 import copy
 
-class JointTrajectoryDequeue:
+class JointTrajectoryReferenceTracker:
 
     def __init__(self):
-        # UAV publishers for trajectory
-        self.joint_trajectory_point_pub = rospy.Publisher('joint_trajectory_point', 
+        # Joint trajectory point is only published while trajectory is non-empty
+        self.joint_trajectory_point_pub = rospy.Publisher(
+            'reference_tracker/joint_trajectory_point', 
             JointTrajectoryPoint, queue_size=1)
-        self.executing_trajectory_pub = rospy.Publisher('executing_trajectory', 
-            Int32, queue_size=1)
+        # Flag that publishes while trajectory is dequeued
+        self.executing_trajectory_pub = rospy.Publisher(
+            'reference_tracker/executing_trajectory', Int32, queue_size=1)
+        # Current reference is continuous publisher
+        self.current_joint_trajectory_reference_pub = rospy.Publisher(
+            'reference_tracker/current_reference',
+            JointTrajectoryPoint, queue_size=1)
 
+        # Parameters
         self.rate = rospy.get_param('~rate', 100)
         self.ros_rate = rospy.Rate(self.rate) 
         
@@ -23,13 +30,19 @@ class JointTrajectoryDequeue:
         self.joint_trajectory = JointTrajectory()
         self.current_trajectory_point = JointTrajectoryPoint()
 
-        rospy.Subscriber('joint_trajectory', JointTrajectory, 
+        # Subscriber for the full joint trajectory
+        rospy.Subscriber('reference_tracker/joint_trajectory', JointTrajectory, 
             self.jointTrajectoryCallback, queue_size=1)
+        # Setting current point initially or overriding it completely.
+        rospy.Subscriber('reference_tracker/set_current_trajectory_point', 
+            JointTrajectoryPoint, self.setCurrentPointCallback, queue_size=1)
 
     def run(self):
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             rate.sleep()
+            self.current_joint_trajectory_reference_pub.publish(
+                self.current_trajectory_point)
 
             if self.executing_trajectory_flag == True:
                 self.executing_trajectory_pub.publish(1)
@@ -52,6 +65,9 @@ class JointTrajectoryDequeue:
         else:
             print "Currently executing a trajectory."
 
+    def setCurrentPointCallback(self, msg):
+        self.current_trajectory_point = msg
+
 
     def publishAll(self):
         self.joint_trajectory_point_pub.publish(self.current_trajectory_point)
@@ -59,7 +75,7 @@ class JointTrajectoryDequeue:
 
 if __name__ == '__main__':
 
-    rospy.init_node('joint_trajectory_dequeue')
-    trajectory_to_ref = JointTrajectoryDequeue()
-    trajectory_to_ref.run()
+    rospy.init_node('joint_trajectory_reference_tracker')
+    reference_tracker = JointTrajectoryReferenceTracker()
+    reference_tracker.run()
 
